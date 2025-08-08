c704aba4f0c63960db62fd04976168b9030a2509
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-08 02:06:12 -0600

    fix: comment out unused chroma-js import in utils.tsx

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,6 +1,7 @@
 // lib/utils.ts
 import { clsx, ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+// import chroma from "chroma-js";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(...inputs));
