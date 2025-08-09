8d6f557e3fb7d1841fb03315b261b068514394b3
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-08 19:57:35 -0600

    feat: add mergeHandler utility function for enhanced event handling

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,8 +1,19 @@
 // lib/utils.ts
 import { clsx, ClassValue } from "clsx";
+import { SyntheticEvent } from "react";
 import { twMerge } from "tailwind-merge";
 // import chroma from "chroma-js";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(...inputs));
+}
+
+export function mergeHandler<T extends SyntheticEvent>(
+  slotHandlers?: (e: T) => void,
+  childHandlers?: (e: T) => void
+) {
+  return (e: T) => {
+    childHandlers?.(e);
+    if (!e.isPropagationStopped?.()) slotHandlers?.(e);
+  };
 }
